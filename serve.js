6e6afa7e5b188d8174f60ac62aa19291aa663f16
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-11-14 10:29:35 -0300

    npm posgres adicionado

--- serve.js
+++ serve.js
@@ -11,23 +11,37 @@ server.post('/videos', (request , reply) => {
         description: 'esse e o video 01',
         duration: 180,
     })
-
-
-
     return reply.status(201).send()
 
 }) 
 
-server.get('/videos', () =>{
-    return 'xd?'
+server.get('/videos', (request) =>{
+    const search = request.query
+
+    console.log(search)
+
+    const videos = database.list()
+    return videos
 })
 
-server.put('/videos/:id', () =>{
-    return 'ai que absurada'
+server.put('/videos/:id', (request) =>{
+    const videoId = request.params.id
+    const{title, description, duration} = request.body
+
+    const video = database.update(videoId, {
+        title,
+        description,
+        duration,
+    }) 
+
+    return reply.status(204).send()
 })
 
-server.delete('/videos/:id', () =>{
-    return 'meu dedo e de amoeba'
+server.delete('/videos/:id', (request, reply) =>{
+    const videoID = request.params.id
+
+    database.delete(videoID)
+    return reply.status(204).send()
 }) 
 
 server.listen({
